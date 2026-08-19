b3302640188ea213bfd1e0f20de946803a4c7532
R-veesh <raweeshalakruvan15@gmail.com>
2026-08-19 14:50:05 +0530

    feat: initialize client-side hydration for TanStack Start application

--- src/client.tsx
+++ src/client.tsx
@@ -1,17 +1,11 @@
 import React from "react";
-import { createRoot } from "react-dom/client";
-import { RouterProvider } from "@tanstack/react-router";
-import { getRouter } from "./router";
+import { hydrateRoot } from "react-dom/client";
+import { StartClient } from "@tanstack/react-start/client";
 import "./styles.css";
 
-const router = getRouter();
-const rootElement = document.getElementById("root")!;
-
-if (!rootElement.innerHTML) {
-  const root = createRoot(rootElement);
-  root.render(
-    <React.StrictMode>
-      <RouterProvider router={router} />
-    </React.StrictMode>
-  );
-}
+hydrateRoot(
+  document,
+  <React.StrictMode>
+    <StartClient />
+  </React.StrictMode>
+);
